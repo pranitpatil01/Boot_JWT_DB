f0e8ea0b7b68efb1d41a8a3677c7a204f9396432
pranitpatil01 <45588565+pranitpatil01@users.noreply.github.com>
2022-11-09 15:33:45 +0530

    Update SwaggerConfiguration.java

--- src/main/java/com/secure/be/SwaggerConfiguration.java
+++ src/main/java/com/secure/be/SwaggerConfiguration.java
@@ -14,23 +14,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
-* http://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
-*/
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 	@Bean
-	public Docket api() {
-//		 ParameterBuilder aParameterBuilder = new ParameterBuilder();
-//	        aParameterBuilder.name("Authorization")                 // name of header
-//	                         .modelRef(new ModelRef("string"))
-//	                         .parameterType("header")               // type - header
-//	                         .required(true)                // for compulsory
-//	                         .build();
-//	        List<Parameter> aParameters = new ArrayList<>();
-//	        aParameters.add(aParameterBuilder.build()); 
-		
+	public Docket api() {		
 		return new Docket(DocumentationType.SWAGGER_2).select() //
 				.apis(RequestHandlerSelectors.any()) //
 				.paths(PathSelectors.any()) //
